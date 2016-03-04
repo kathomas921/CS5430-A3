1d6344ca6a1460149737522bb2d3e665413b38de
Katie Thomas <kathomas@go.wustl.edu>
2016-03-04 13:33:15 -0500

    why isnt this working

--- bob.py
+++ bob.py
@@ -2,7 +2,7 @@ import sys
 import socket
 import SocketServer
 
-class MyTCPHandler(SocketServer.BaseRequestHandler):
+class MyTCPHandler(SocketServer.StreamRequestHandler):
     """
     The request handler class for our server.
 
@@ -14,14 +14,16 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         # self.rfile is a file-like object created by the handler;
         # we can now use e.g. readline() instead of raw recv() calls
+        print("bob handle 1")
         self.data = self.rfile.readline().strip()
         print "{} wrote:".format(self.client_address[0])
         print self.data
+        print("bob handle 4")
 
         # KT: I don't think we need this
         # # Likewise, self.wfile is a file-like object used to write back
         # # to the client
-        # self.wfile.write(self.data.upper())
+        self.wfile.write("%d bytes successfuly received." % (len(self.data)))
 
 
 if __name__ == "__main__":
@@ -29,7 +31,7 @@ if __name__ == "__main__":
     # HOST, PORT = "localhost", 9999
 
     # Create the server, binding to localhost on port 9999
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+    server = SocketServer.TCPServer(('localhost', 9998), MyTCPHandler)
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
