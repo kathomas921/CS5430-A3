90898a415ae2777b01125a46eef7523d97e0cc27
jrg365 <jrg365@nikola-compute01@coecis.cornell.edu>
2016-03-07 15:00:25 -0500

    Add Alice, add python notebook

--- Alice.py
+++ Alice.py
@@ -4,7 +4,13 @@ import argparse
 import json
 import signal
 from Message import Message
-
+import Crypto.Random.random as cryptrand
+import time
+def generate_signed_key_string(plaintext_key, asymcrypt):
+    key_exchange_message = {'message_type': 'key_exchange', 'recipient': 'Bob', 'symmetric_key': asymcrypt.encrypt(plaintext_key), 'timestamp': time.time()}
+    key_exchange_str = json.dumps(key_exchange_message)
+    signed_message = {'message': key_exchange_str, 'signature': asymcrypt.sign(key_exchange_str)}
+    return json.dumps(signed_message)
 
 def signal_handler(signal,frame):
     global sock
@@ -17,24 +23,29 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='parser test')
     parser.add_argument('-p', '--port', help="port number of receiving host", dest='port', type=int, required=True)
     parser.add_argument('-a', '--address', help="hostname or IPV4 address of receiving host", dest='addr', type=str, required=True)
-    #parser.add_argument('-e', '--encryption', help="type of encryption", dest='enc', type=str, required=True)
-
+    parser.add_argument('-e', '--encryption', help="Encrypt messages using AES(0/1)", dest='enc', type=int, required=False)
+    parser.add_argument('-s', '--signing', help="Sign messages (0/1)", dest='sign', type=int, required=False)
     args = parser.parse_args()
     print args.addr
 
-
-   
-    # HOST, PORT = "localhost", 9999
-    # data = " ".join(sys.argv[1:])
-
-    # Create a socket (SOCK_STREAM means a TCP socket)
+    AliceAsym = AsymmetricIMCrypto('Keys/Public/Bob_public_key.txt','Keys/Alice_private_key.txt')
+    
+    symmetric_key = str(bytearray([cryptrand.getrandbits(8) for i in xrange(0,16)]))    
+    hmac_key = str(bytearray([cryptrand.getrandbits(8) for i in xrange(0,16)]))
+    SymCrypt = SymmetricIMCrypto(symmetric_key)
+    SymSigner = SymmetricIMSigner(hmac_key) 
+     
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     signal.signal(signal.SIGINT, signal_handler)
+
+    if args.enc:
+        msg = generate_signed_key_string(
+
     MSG_COUNT = 0
     try:
-        # Connect to server and send data
-        # This should be recipient (Mallory's) host/port
         sock.connect((args.addr, args.port))
+                
+
         while True:
             msg = raw_input("Message (empty string to quit): ")
             if msg == "":
